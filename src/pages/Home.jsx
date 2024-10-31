93ab85b8bc38709038ea0951a4326280135d40dc
Cecep <cecepsolihinyusup@gmail.com>
2024-10-31 14:17:27 +0700

    feat: add useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+import Navbar from "../components/Navbar"
 
 const Home = () => {
+    const [bannerData, setBanner] = useState([])
+
+    useEffect(() => {
+        fetch('https://reverb.simulasi-minat-bakat-siswa.hitungdong.online/api/banners', {
+            headers: {
+                accept: 'Application/json'
+            }
+        }).then(async (result) => {
+            const data = await result.json()
+            console.log(data)
+        })
+    }, [])
+
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+        </>
     )
 }
 
